1cb48984f8088a503c6ddf20aeab333633961d31
jparchure <jayant-parchure@uiowa.edu>
2013-11-21 17:43:52 -0600

    Migration Created

--- app/models/compose_tweet.rb
+++
@@ -1,7 +0,0 @@
-class ComposeTweet < ActiveRecord::Base
-  attr_accessible :id, :text, :author
-  
-  def self.create_compose_tweet (id, text, author)
-    ComposeTweet.create(:id=>id, :text=>text, :author=>author)
-  end
-end
--- db/migrate/20131105224218_add_session_token_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddSessionTokenToUsers < ActiveRecord::Migration
-  def change
-    add_index :users, :session_token
-  end
-end
