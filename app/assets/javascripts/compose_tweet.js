15531bb8168faa63dea6dc6aa284d467fda96f3f
Audrey Altman <audrey-altman@uiowa.edu>
2013-11-20 22:06:04 -0800

    compose tweets index

--- app/assets/javascripts/compose_tweet.js
+++ app/assets/javascripts/compose_tweet.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   console.log('compose tweet js recognized');
-  $('#compose_tweet').html('<img src="compose.jpg", alt="compose tweet" />').click(function(){
-    console.log('compose tweet clicked');
-  });
+  //$('#compose_tweet').html('<img src="compose.jpg", alt="compose tweet" />').click(function(){
+  //  console.log('compose tweet clicked');
+  //});
 });
 
 
