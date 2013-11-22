d55d3022df3b57107d7e2b3d36dc0bef211cc9f2
Audrey Altman <audrey-altman@uiowa.edu>
2013-11-21 19:26:08 -0800

    compose tweet controller

--- spec/controllers/compose_tweets_controller_spec.rb
+++ spec/controllers/compose_tweets_controller_spec.rb
@@ -1,29 +1,61 @@
 require 'spec_helper'
 
 describe ComposeTweetsController do
+
   describe 'adding compose tweet' do
-    before :each do
-      @fake_data = {:compose_tweet=>{'text'=>'tweet text'}}
-      @fake_tweet = double()
-    end
-    describe 'valid input' do
-      pending 'call to Twitter'
-      it 'should use the Twitter gem to compose a tweet' do 
-        Twitter.should_receive(:update).with('tweet text').and_return(@fake_tweet)
-        post :create, @fake_data
+    
+   before :each do
+     @current_user = double()
+     User.stub(:find_by_session_token).and_return(@current_user)
+   end
+  
+    describe 'given valid input' do
+      before :each do
+        @valid_input = {:compose_tweet => {'text' => 'hello'}}
+        @twitter_obj = double()
+      end
+      it 'should call the Twitter gem to compose a tweet' do 
+        Twitter.should_receive(:update).with('hello').and_return(@twitter_obj)
+        @twitter_obj.stub(:id).and_return('fake_id')
+        @twitter_obj.stub(:text).and_return('fake_text')
+        @current_user.stub(:name).and_return('fake_name')
+        ComposeTweet.stub(:create_compose_tweet)
+        post :create, @valid_input
+        assigns(:tweet_text).should == 'hello'
       end
       it 'should call the model method to create a new compose tweet record' do
-        pending 'call to Twitter'
-        #Twitter.should_receive(:update).and_return(@fake_tweet)
-        #ComposeTweet.should_receive(:create_compose_tweet).with(@fake_tweet[:id], @fake_tweet[:text], "Mr. Magoo")
-        #post :create, @fake_data       end
-      it 'should redirect to the streams path'
+        Twitter.stub(:update).and_return(@twitter_obj)
+        @twitter_obj.stub(:id).and_return('fake_id')
+        @twitter_obj.stub(:text).and_return('fake_text')
+        @current_user.stub(:name).and_return('fake_name')
+        ComposeTweet.should_receive(:create_compose_tweet).with('fake_id', 'fake_text', 'fake_name')
+        post :create, @valid_input      
+      end
+      it 'should redirect to the streams path' do
+        Twitter.stub(:update).and_return(@twitter_obj)
+        @twitter_obj.stub(:id).and_return('fake_id')
+        @twitter_obj.stub(:text).and_return('fake_text')
+        @current_user.stub(:name).and_return('fake_name')
+        ComposeTweet.stub(:create_compose_tweet)
+        post :create, @valid_input
+        response.should redirect_to ('/streams')
+      end
     end
-    describe 'invalid input' do
+    describe 'given invalid input' do
+      before :each do
+       @invalid_input = {:compose_tweet=>{'text'=>''}}
+      end
       it 'should show a flash message' do
-        
-        post :create, {:compose_tweet=>{'text'=>''}
+        post :create, @invalid_input
+        flash[:notice].should == 'Please compose a tweet between 1 and 140 characters'
+      end
+      it 'should redirect to new compose tweet path' do
+        post :create, @invalid_input
+        response.should redirect_to ('/compose_tweets/new')
       end
     end
   end
+
+
+
 end
