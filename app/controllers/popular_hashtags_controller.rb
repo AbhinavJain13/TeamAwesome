c47d710018005a00cab6cf4dfd3c775e428252b4
Audrey Altman <audrey-altman@uiowa.edu>
2013-12-08 10:44:48 -0800

    show popular tweets controller and spec

--- app/controllers/popular_hashtags_controller.rb
+++ app/controllers/popular_hashtags_controller.rb
@@ -1,9 +1,8 @@
 class PopularHashtagsController < ApplicationController
 
-before_filter :set_current_user
+before_filter :set_current_user, :get_hashtags
 
-  def index
-    @current_twitter_user = Twitter.user
+  def get_hashtags 
     timeline = Twitter.home_timeline
     @hashtags = {}
     timeline.each do |tweet|
@@ -15,15 +14,31 @@ before_filter :set_current_user
             break
           end
           if tag == hashtag
-            @hashtags[hashtag].push(tweet.id)
+            @hashtags[hashtag].push(tweet)
             match = true
           end
         end
         if match == false
-          @hashtags[tag] = [tweet.id]
+          @hashtags[tag] = [tweet]
         end
       end
     end
     @hashtags = @hashtags.sort_by{|key, value| value.length}.reverse 
+  end
+
+  def index
+    @current_twitter_user = Twitter.user
+  end
+
+  def show
+    @current_twitter_user = Twitter.user
+    tag = params[:id] 
+    @tweets = []
+    @hashtags.each do |hashtag|
+      if hashtag[0] == tag 
+        @tweets = hashtag[1]
+        break
+      end
+    end
   end
 end
